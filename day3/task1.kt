acbfb5c58614f75ab9a7d5b943cf74dd8563453e
MaciPaci <maciej.stachowiak420@gmail.com>
2023-12-03 21:09:33 +0100

    Day 3

---
+++ day3/task1.kt
@@ -0,0 +1,63 @@
+import java.io.File
+import java.io.InputStream
+import java.nio.file.Files
+import java.nio.file.Paths
+
+data class Number (
+    var value: Int,
+    val row: Int,
+    val column: Int,
+    var length: Int,
+    var nextToSymbol: Boolean = false
+)
+
+fun main() {
+    val filePath = "input.txt"
+    val patternNumbers = "\\d+".toRegex()
+    val numbersList = mutableListOf<Number>()
+    val file = File(filePath)
+    val lineLength = file.bufferedReader().use { it.readLine() }.length
+    val lineCount = Files.lines(Paths.get(filePath)).count().toInt()
+    val inputStream: InputStream = file.inputStream()
+    val inputString = inputStream.bufferedReader().use { it.readText() }
+    val inputStringNoNewline = inputString.replace("\n", "")
+    var sumOfNumbers = 0
+
+    patternNumbers.findAll(inputString).forEach { result ->
+        val number = result.value.toInt()
+        val range = result.range
+
+        val newNumber = Number(
+            number,
+            range.first / (lineLength+1),
+            range.first % (lineLength+1),
+            range.count(),
+        )
+        numbersList.add(newNumber)
+    }
+
+    for (number in numbersList) {
+        if (isSymbolNearNumber(inputStringNoNewline, number, lineLength, lineCount)) {
+            number.nextToSymbol = true
+            sumOfNumbers += number.value
+        }
+    }
+    println("Sum of numbers with symbol adjacent: $sumOfNumbers")
+}
+
+fun isSymbolNearNumber(inputString: String, number: Number, lineLength: Int, lineCount: Int): Boolean {
+    val patternSymbols = "[^.\\d\\n]+".toRegex()
+    var windowString = ""
+    for (i in number.row-1..number.row+1) {
+        if (i < 0 || i >= lineCount) {
+            continue
+        }
+        for (j in number.column-1..number.column+number.length) {
+            if (j < 0 || j >= lineLength) {
+                continue
+            }
+            windowString += inputString[i*lineLength+j]
+        }
+    }
+    return patternSymbols.containsMatchIn(windowString)
+}
---
+++ day3/task2.kt
@@ -0,0 +1,81 @@
+import java.io.File
+import java.io.InputStream
+import java.nio.file.Files
+import java.nio.file.Paths
+
+data class Number (
+    var value: Int,
+    val row: Int,
+    val column: Int,
+    var length: Int,
+    var starsIndexes: MutableList<Int> = mutableListOf()
+)
+
+data class Star (
+    var index: Int,
+    var adjacentNumbers: MutableList<Number> = mutableListOf()
+)
+
+fun main() {
+    val filePath = "input.txt"
+    val patternNumbers = "\\d+".toRegex()
+    val patternStars = "[*]".toRegex()
+    val numbersList = mutableListOf<Number>()
+    val starsMap = mutableMapOf<Int, Star>()
+    val file = File(filePath)
+    val lineLength = file.bufferedReader().use { it.readLine() }.length
+    val lineCount = Files.lines(Paths.get(filePath)).count().toInt()
+    val inputStream: InputStream = file.inputStream()
+    val inputString = inputStream.bufferedReader().use { it.readText() }
+    val inputStringNoNewline = inputString.replace("\n", "")
+    var sumOfNumbers = 0
+
+    patternNumbers.findAll(inputString).forEach { result ->
+        val number = result.value.toInt()
+        val range = result.range
+
+        val newNumber = Number(
+            number,
+            range.first / (lineLength + 1),
+            range.first % (lineLength + 1),
+            range.count(),
+        )
+        numbersList.add(newNumber)
+    }
+
+    patternStars.findAll(inputStringNoNewline).forEach { star ->
+        starsMap[star.range.first] = Star(star.range.first)
+    }
+
+    for (number in numbersList) {
+        number.starsIndexes = findStarsNearNumber(inputStringNoNewline, number, lineLength, lineCount)
+        number.starsIndexes.forEach { starIndex ->
+            starsMap[starIndex]?.adjacentNumbers?.add(number)
+        }
+    }
+    starsMap.forEach { star ->
+        if (star.value.adjacentNumbers.size == 2) {
+            sumOfNumbers += star.value.adjacentNumbers[0].value * star.value.adjacentNumbers[1].value
+        }
+    }
+
+    println("Sum of numbers with symbol adjacent: $sumOfNumbers")
+}
+
+fun findStarsNearNumber(inputString: String, number: Number, lineLength: Int, lineCount: Int): MutableList<Int> {
+    val starsIndexes = mutableListOf<Int>()
+    for (i in number.row-1..number.row+1) {
+        if (i < 0 || i >= lineCount) {
+            continue
+        }
+        for (j in number.column - 1..number.column + number.length) {
+            if (j < 0 || j >= lineLength) {
+                continue
+            }
+            if (inputString[i * lineLength + j] == '*') {
+                starsIndexes.add(i * lineLength + j)
+            }
+        }
+    }
+    return starsIndexes
+}
