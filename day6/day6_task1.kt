a6d85134ec83d2e9dfd222dcaa4d9968e29a717c
MaciPaci <maciej.stachowiak420@gmail.com>
2023-12-06 12:07:07 +0100

    Day 6

---
+++ day6/day6_task1.kt
@@ -0,0 +1,39 @@
+import java.io.File
+import java.io.InputStream
+
+data class Race(
+    var time: Int,
+    var record: Int,
+    var racesWon: Int = 0
+)
+
+fun main() {
+    val filePath = "./day6/input.txt"
+    val pattern = "(?<=\\:).*".toRegex()
+    val file = File(filePath)
+    val inputStream: InputStream = file.inputStream()
+    val inputString = inputStream.bufferedReader().use { it.readText() }
+
+    val match = pattern.findAll(inputString).map { it.value }.map { it.split(' ') }.map { it.filter { it.isNotBlank() }}.toList()
+    val timeTable = match[0].map { it.toInt() }
+    val distanceTable = match[1].map { it.toInt() }
+
+    val raceTable = mutableListOf<Race>()
+    for (i in timeTable.indices) {
+        raceTable.add(Race(timeTable[i], distanceTable[i]))
+    }
+
+    var velocity: Int
+    var distance: Int
+
+    raceTable.forEach { race ->
+        for (t in 1..<race.time) {
+            velocity = t
+            distance = velocity*(race.time-t)
+            if (distance > race.record) {
+                race.racesWon += 1
+            }
+        }
+    }
+    println("Races won: ${raceTable.fold(1){ acc: Int, race: Race -> acc * race.racesWon }}")
+}
---
+++ day6/day6_task2.kt
@@ -0,0 +1,28 @@
+import java.io.File
+import java.io.InputStream
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+data class OneRace(
+    var time: Long,
+    var record: Long,
+)
+
+fun main() {
+    val filePath = "./day6/input.txt"
+    val file = File(filePath)
+    val inputStream: InputStream = file.inputStream()
+    val inputString = inputStream.bufferedReader().use { it.readText() }
+
+    val raceData = inputString.trim().split('\n').map { it.filter { it.isDigit() }.toLong()}
+
+    val race = OneRace(raceData[0], raceData[1])
+
+    val delta = race.time.toDouble().pow(2.0) - 4*race.record
+    val t1 = (-race.time - sqrt(delta))/-2
+    val t2 = (-race.time + sqrt(delta))/-2
+
+    val racesWon = t1.toInt()-t2.toInt()
+
+    println("Races won: $racesWon")
+}
